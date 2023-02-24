c9eb888c4112624cfd50fc27010693d4ca15799d
chengjiangg <chengjiang98@gmail.com>
2023-02-24 10:56:28 +0800

    Refactor code

--- User_Interface/MyApp.py
+++ User_Interface/MyApp.py
@@ -4,59 +4,82 @@ import plotly.express as px
 import base64
 import io
 
-# Title of the app
-st.title("Multi-Lingual Text Classification")
 
-# Add a sidebar
-st.sidebar.subheader("Classification Settings")
+def load_dataset():
+    # Add a sidebar
+    st.sidebar.subheader("Classification Settings")
+    # Select language
+    langauge = st.sidebar.selectbox(
+        "Select language",
+        ("English", "Chinese")
+    )
+    # Setup file upload
+    uploaded_file = st.sidebar.file_uploader(
+        label="Upload your Excel file.", type=['xlsx'])
+    if uploaded_file is not None:
+        try:
+            # Read excel file
+            df = pd.read_excel(uploaded_file)
+            return df
+        except Exception as e:
+            print(e)
+    else:
+        return None
 
-# Select language
-langauge = st.sidebar.selectbox(
-    "Select language",
-    ("English", "Chinese")
-)
 
-# Setup file upload
-uploaded_file = st.sidebar.file_uploader(
-    label="Upload your Excel file.", type=['xlsx'])
+def predict(df):
+    # Get counts for emotion
+    emotion_count = df['emotion'].value_counts().rename_axis(
+        'emotion').reset_index(name="counts")
+    # Get counts for stance
+    stance_count = df['stance'].value_counts().rename_axis(
+        'stance').reset_index(name="counts")
+    return emotion_count, stance_count
 
-if uploaded_file is not None:
-    try:
-        # Read excel file
-        df = pd.read_excel(uploaded_file)
-        # Get counts for emotion
-        emotion_count = df['emotion'].value_counts().rename_axis(
-            'emotion').reset_index(name="counts")
-        # Get counts for stance
-        stance_count = df['stance'].value_counts().rename_axis(
-            'stance').reset_index(name="counts")
-        # Plot counts for emotion
-        emotion_fig = px.bar(emotion_count, x="emotion", y="counts",
-                             color="emotion", text="counts", title="Emotion Counts", labels={
-                                 "emotion": "Emotion",
-                                 "counts": "Counts",
-                             })
-        # Plot counts for stance
-        stance_fig = px.bar(stance_count, x="stance", y="counts",
-                            color="stance", text="counts", title="Stance Counts", labels={
-                                "stance": "Stance",
-                                "counts": "Counts",
-                            })
+
+def plot_fig(df, xCol, yCol, title, labels):
+    fig = px.bar(df, x=xCol, y=yCol,
+                 color=xCol, text=yCol, title=title, labels=labels)
+    return fig
+
+
+def export_to_excel(df):
+    towrite = io.BytesIO()
+    df.to_excel(
+        towrite, encoding='utf-8', index=False, header=True)
+    towrite.seek(0)
+    b64 = base64.b64encode(towrite.read()).decode()
+    linko = f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64}" download="Results.xlsx">Export Result</a>'
+    return linko
+
+
+def main():
+    # Title of the app
+    st.title("Multi-Lingual Text Classification")
+    dataset = load_dataset()
+    result = st.sidebar.button("Run")
+    if dataset is not None and result:
+        # Get counts of emotion and stance of prediction
+        emotion_count, stance_count = predict(dataset)
+        # Plot bar chart for emotion
+        emotion_labels = {"emotion": "Emotion", "counts": "Counts"}
+        emotion_fig = plot_fig(emotion_count, "emotion",
+                               "counts", "Emotion Counts", emotion_labels)
+        # Plot bar chart for stance
+        stance_labels = {"stance": "Stance", "counts": "Counts"}
+        stance_fig = plot_fig(stance_count, "stance",
+                              "counts", "Stance Counts", stance_labels)
+        # Start of Output
         st.header('Label Statistics')
         st.plotly_chart(emotion_fig)
         st.plotly_chart(stance_fig)
-
         st.header('Output')
-        st.write(df)
-        # Export results to excel
-        towrite = io.BytesIO()
-        downloaded_file = emotion_count.to_excel(
-            towrite, encoding='utf-8', index=False, header=True)
-        towrite.seek(0)
-        b64 = base64.b64encode(towrite.read()).decode()
-        linko = f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64}" download="Results.xlsx">Export Result</a>'
+        st.write(dataset)
+        linko = export_to_excel(dataset)
         st.markdown(linko, unsafe_allow_html=True)
-    except Exception as e:
-        print(e)
-else:
-    st.write("Please upload an excel file to the application")
+    else:
+        st.write("Please upload an excel file to the application")
+
+
+if __name__ == '__main__':
+    main()

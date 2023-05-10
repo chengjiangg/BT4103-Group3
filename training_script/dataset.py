28501c4577ec759619c14ce434a7e9f4a5ae9cd2
zeninggg <65763493+zeninggg@users.noreply.github.com>
2023-05-10 16:23:48 +0800

    Update dataset.py

--- training_script/dataset.py
+++ training_script/dataset.py
@@ -154,12 +154,12 @@ class ZH_SocialMediaDS(Dataset):
         if len(tokens) > self.max_token_length:
             # get index of entity in comment
             # some entity is companied with puncatuations i.e. boarders
-            entity_ind = None
+            #entity_ind = None
             for i, word in enumerate(tokens):
                 if "[ENTITY]" in word:
                     entity_ind = i
                     break
-            assert entity_ind == None, f"{row['entity']} does not exist in {row['text']}"
+            #assert entity_ind == None, f"{row['entity']} does not exist in {row['text']}"
             start = max(entity_ind - int(self.max_token_length/2), 0)
             end = start + self.max_token_length
             comment = tokens[start:end]

9f4cf9b802e6645ec203c501c14a2fe10795ff8f
zeninggg <65763493+zeninggg@users.noreply.github.com>
2023-05-10 12:21:58 +0800

    Update dataset.py

--- training_script/dataset.py
+++ training_script/dataset.py
@@ -159,7 +159,7 @@ class ZH_SocialMediaDS(Dataset):
                 if "[ENTITY]" in word:
                     entity_ind = i
                     break
-            assert entity_ind == None, "{row['entity']} does not exist in {row['text']}"
+            assert entity_ind == None, f"{row['entity']} does not exist in {row['text']}"
             start = max(entity_ind - int(self.max_token_length/2), 0)
             end = start + self.max_token_length
             comment = tokens[start:end]
